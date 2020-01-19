e4505de1705b52349d35f45ede352e46f6afd584
Victor Augusto <38368198+victoraugustoas@users.noreply.github.com>
2020-01-18 20:05:32 -0400

    Create install_packages.sh

---
+++ install_packages.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# apps
+flatpak install flathub com.spotify.Client -y
+flatpak install flathub com.slack.Slack -y
+flatpak install flathub com.visualstudio.code -y
+flatpak install flathub com.getpostman.Postman -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.jetbrains.WebStorm -y
+flatpak install flathub com.axosoft.GitKraken -y
+flatpak install flathub org.telegram.desktop -y
+
+# git, zsh and curl
+sudo apt install git zsh curl
+
+# oh-my-zsh and plugins
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)" # zplugin
+
+# fzf-zsh
+git clone https://github.com/junegunn/fzf.git ${ZSH}/custom/plugins/fzf
+${ZSH}/custom/plugins/fzf/install --bin
+git clone https://github.com/Treri/fzf-zsh.git ${ZSH}/custom/plugins/fzf-zsh
+
+# get .zshrc
+curl https://raw.githubusercontent.com/victoraugustoas/zsh-pc/master/.zshrc > $HOME/.zshrc
